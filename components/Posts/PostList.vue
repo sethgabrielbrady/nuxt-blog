8beed1aabc21cf2dad6c9cab829fb2d82df620ee
Seth Brady <seth.brady@framebridge.com>
2021-01-18 21:03:48 -0500

    next blog first commit

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="posts-page">
+        <section class='post-lists'>
+            <PostPreview
+                id='1'
+                :isAdmin="isAdmin"
+                title='First Post'
+                thumbnail='https://www.brookings.edu/wp-content/uploads/2017/11/metro_20171121_tech-empowers-tech-polarizes-mark-muro.jpg'
+                previewText='Some text here'
+            />
+            <PostPreview
+                id='2'
+                :isAdmin="isAdmin"
+                title='Second Post'
+                thumbnail='https://www.brookings.edu/wp-content/uploads/2017/11/metro_20171121_tech-empowers-tech-polarizes-mark-muro.jpg'
+                previewText='More text here'
+            />
+            <PostPreview
+                id='3'
+                :isAdmin="isAdmin"
+                title='Third Post'
+                thumbnail='https://www.brookings.edu/wp-content/uploads/2017/11/metro_20171121_tech-empowers-tech-polarizes-mark-muro.jpg'
+                previewText='Again, more text here'
+            />
+        </section>
+    </div>
+</template>
+
+<script>
+    import PostPreview from '@/components/Posts/PostPreview'
+    export default {
+        components: {
+            PostPreview,
+        },
+        props: {
+            isAdmin: {
+                type: Boolean,
+                default: false
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .post-lists {
+        display: flex;
+        padding: 20px;
+        box-sizing: border-box;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="admin-post-page">
+        <section class="update-form">
+            <AdminPostForm :post="loadedPost" />
+        </section>
+    </div>
+</template>
+
+<script>
+    import AdminPostForm from '@/components/Admin/AdminPostForm'
+    export default {
+        layout: 'admin',
+        components: {
+            AdminPostForm
+        },
+        data() {
+            return {
+                loadedPost: {
+                    author: "Seth Brady",
+                    title: "An Amazing Post",
+                    thumbnaiink:'https://www.brookings.edu/wp-content/uploads/2017/11/metro_20171121_tech-empowers-tech-polarizes-mark-muro.jpg',
+                    content: "The best content ever, here"
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .update-form {
+    width: 90%;
+    margin: 20px auto;
+    }
+    @media (min-width: 768px) {
+    .update-form {
+        width: 500px;
+    }
+    }
+</style>
